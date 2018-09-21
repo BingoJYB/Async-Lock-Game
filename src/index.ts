ff1cba1377bcb17bb565509e6935ee1cbd10fe11
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-09-22 00:21:01 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-let AsyncLock = require('async-lock');
-let lock = new AsyncLock();
+let AsyncLock = require('async-lock')
+let lock = new AsyncLock()
 
 async function sleep(ms: number) {
     return new Promise((resolve: any) => {
@@ -8,22 +8,16 @@ async function sleep(ms: number) {
 }
 
 async function randomDelay() {
-    const randomTime = Math.round(Math.random() * 10000);
+    const randomTime = Math.round(Math.random() * 10000)
     return sleep(randomTime)
 }
 
 class ShipmentSearchIndex {
      async updateShipment(id: string, shipmentData: any) {
-        const startTime = new Date();
+        const startTime = new Date()
         await randomDelay();
-        const endTime = new Date();
-        console.log(`update ${id}@${
-            startTime.toISOString()
-            } with ${shipmentData} finished@${
-            endTime.toISOString()
-            }`
-        );
-
+        const endTime = new Date()
+        console.log(`update ${id}@${startTime.toISOString()} with ${shipmentData} finished@${endTime.toISOString()}`)
         return {startTime, endTime}
     }
 }
@@ -35,18 +29,17 @@ interface ShipmentUpdateListenerInterface {
 class ShipmentUpdateListener extends ShipmentSearchIndex implements ShipmentUpdateListenerInterface {
     receiveUpdate(id: string, shipmentData: any): void {
         lock.acquire(id, async () => {
-            await this.updateShipment(id, shipmentData);
+            await this.updateShipment(id, shipmentData)
         })
     }
 }
 
-let shipment: ShipmentUpdateListenerInterface = new ShipmentUpdateListener();
-
-shipment.receiveUpdate('1', 'data1');
-shipment.receiveUpdate('1', 'data2');
-shipment.receiveUpdate('2', 'data1');
-shipment.receiveUpdate('1', 'data3');
-shipment.receiveUpdate('1', 'data4');
-shipment.receiveUpdate('2', 'data2');
-shipment.receiveUpdate('3', 'data1');
-shipment.receiveUpdate('2', 'data3');
+let shipment: ShipmentUpdateListenerInterface = new ShipmentUpdateListener()
+shipment.receiveUpdate('1', 'data1')
+shipment.receiveUpdate('1', 'data2')
+shipment.receiveUpdate('2', 'data1')
+shipment.receiveUpdate('1', 'data3')
+shipment.receiveUpdate('1', 'data4')
+shipment.receiveUpdate('2', 'data2')
+shipment.receiveUpdate('3', 'data1')
+shipment.receiveUpdate('2', 'data3')
